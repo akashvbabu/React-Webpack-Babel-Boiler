8212ec1d51d5c7e3bffbf3a1602bb4a774631f6a
Akash Vishwanath Babu <akash.v.babu@gmail.com>
2016-01-23 12:02:04 -0500

    First Commit

---
+++ Hello.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Akash on 1/23/2016.
+ */
+var React = require('react');
+
+module.exports = React.createClass({
+   displayName: 'Hello React',
+
+    render: function(){
+        return(
+            <div>
+                Hello React
+                </div>
+        );
+    }
+});
---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Akash on 1/23/2016.
+ */
+var React = require('react');
+var Hello = require('./Hello.js');
+
+React.render(<Hello />, document.getElementById('content'));
